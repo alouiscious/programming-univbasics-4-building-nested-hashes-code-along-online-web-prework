f7a6dfae2809caa618a2f4e4b43e956f6876bb6a
Alouiscious Sims <info@reneesoul.com>
2019-07-15 01:49:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,39 +4,4 @@ def first_challenge
      :capulet => {}
    }
 
-#      :montague => {
-#        :patriarch => {
-#          :name => "Lord Montague",
-#          :age => "53"
-#        },
-#        :matriarch => {
-#          :name => "Lady Montague",
-#          :age => "54"
-#        },
-#        :hero => {
-#          :name => "Romeo",
-#          :age => "15"
-#        },
-#        :hero_friends => [
-#          {
-#            :name => "Benvolio",
-#            :age => "17",
-#            :attitude => "worried"
-#          },
-#          {
-#            :name => "Mercutio",
-#            :age => "18",
-#            :attitude => "hot-headed"
-#          }
-#        ]
-#      },
-#      :capulet => {
-#        :patriarch => {
-#          :name => "Lord Capulet",
-#          :age => "50"
-#        },
-#        :matriarch
-#      }
-#        }
-#    }
-# end
+end
