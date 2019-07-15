19f31f55a5048c76977e59fa2866433ab4323e42
Alouiscious Sims <info@reneesoul.com>
2019-07-15 01:16:24 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,42 @@
 def first_challenge
-   epic_tragedy = {}
-end
+   epic_tragedy = {
+     :montague => {},
+     :capulet => {}
+   }
+     
+#      :montague => {
+#        :patriarch => {
+#          :name => "Lord Montague",
+#          :age => "53"
+#        },
+#        :matriarch => {
+#          :name => "Lady Montague",
+#          :age => "54"
+#        },
+#        :hero => {
+#          :name => "Romeo",
+#          :age => "15"
+#        },
+#        :hero_friends => [
+#          {
+#            :name => "Benvolio",
+#            :age => "17",
+#            :attitude => "worried"
+#          },
+#          {
+#            :name => "Mercutio",
+#            :age => "18",
+#            :attitude => "hot-headed"
+#          }
+#        ]
+#      },
+#      :capulet => {
+#        :patriarch => {
+#          :name => "Lord Capulet",
+#          :age => "50"
+#        },
+#        :matriarch
+#      }
+#        }
+#    }
+# end
